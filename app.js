614779d51797ab5bd7b34ae21807dd60dbdf5ac5
Samuel De La Cruz <samuel@codetiger.co>
2023-07-25 01:09:15 -0400

    .

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const controllers = require("./controllers");
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
 app.set("view engine", "vash"); //set view engine
 app.use(express.static(__dirname + "/public"));
 //opt into services
