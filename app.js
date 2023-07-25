a672d2dc456901c78d724f5574431f748e43e47d
Samuel De La Cruz <samuel@codetiger.co>
2023-07-25 00:15:57 -0400

    .

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-require('dotenv').config()
-const http = require('http');
-const port = (process.env.PORT || 5000);
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const http = require("http");
+const port = process.env.PORT || 5000;
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const controllers = require('./controllers');
-app.use(cors())
-app.set("view engine", "vash");//set view engine
+const controllers = require("./controllers");
+app.use(cors());
+app.set("view engine", "vash"); //set view engine
 app.use(express.static(__dirname + "/public"));
 //opt into services
 const flash = require("connect-flash");
-const bodyParser = require('body-parser');
-const expressSession = require('express-session');
+const bodyParser = require("body-parser");
+const expressSession = require("express-session");
 const CookieParser = require("cookie-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(expressSession({secret: "thesecret"}));
+app.use(expressSession({ secret: "thesecret" }));
 app.use(flash());
 app.use(CookieParser());
-const auth = require('./auth');
+const auth = require("./auth");
 auth.init(app);
 
 controllers.init(app);
 
 const server = http.createServer(app);
 
-server.listen(port,() => {
-    console.log(`Listening on port ${port}`)
-} );
-
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
