4ab129d3f57093821565513e3bde919727763477
Samuel De La Cruz <samuel@codetiger.co>
2025-06-08 09:23:45 -0400

    Remove Angular and server files: deleted bower.json, package.json, and server.js to streamline project dependencies and server setup.

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-( (controllers) =>{
-  
-    const bibleController = require("./bibleController")
-    controllers.init =  (app) =>{
-        
-        bibleController.init(app);
-    }
-
-})(module.exports);
--- data/sqliteDb.js
+++
@@ -1,5 +0,0 @@
-const db = require('better-sqlite3')('Bible.db');
-db.pragma('journal_mode = WAL');
-module.exports = {
-    sqliteDb: db
-}
